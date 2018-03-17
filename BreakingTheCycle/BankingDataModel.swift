644e61ecbf3c38be3a50e71b7830ed22edb1d18b
Cosmin <mirceacosmin@gmail.com>
2018-03-17 17:22:03 +0000

    Initial commit

---
+++ BreakingTheCycle/BankingDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherDataModel.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 24/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class BankingDataModel {
+    
+    //Declare your model variables here
+    var name: String = ""
+    
+    //all the rest of the variables we need
+    
+}
+
--- BreakingTheCycle/ViewController.swift
+++ BreakingTheCycle/ViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    //Constants
+    let BANKING_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "0245a2b7f7c30ff1d4a0b028c137c19c" //CHANGE THIS WITH THE ID FOR ACCESSING THE DATA
+    
+    let bankingDataModel = BankingDataModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // getCustomerData(url: BANKING_URL, parameters: params)
+        
     }
+    
+    func getCustomerData(url: String, parameters: [String:String]) {
+        
+        //url where is the request sent
+        // .get is about getting the data
+        // parameters specificed by opendata weahter map specified for parameters lar and long
+        
+        let request = Alamofire.request(url, method: .get, parameters: parameters)
+        
+        //do something with the data coming back
+        let _ = request.responseJSON { (response) in
+            
+            if response.result.isSuccess {
+                print("Success!, got the weather data")
+                
+                //this from SwiftyJSON
+                let bankingJSON: JSON = JSON(response.result.value!) //assign the result value converted to json to the JSON weatherJSON constant
+                
+                self.updateBankingData(json: bankingJSON)
+                
+            } else {
+                //alamofire gives you the eror for the result
+                print("Erros \(response.result.error!)")
 
+            }
+        }
+        
+    }
+    
+    //JSON partins
+    
+    func updateBankingData(json: JSON) {
+
+        //HOW TO USE IT:
+        
+//        if let dataTempResult = json["main"]["transactions"] { //.double changes json in double
+//
+//            bankingDataModel.name = dataTempResult.name
+//
+//        } else {
+//            //POST error
+//
+//        }
+    }
 
 }
 
